e20acc9aa56f0998646f1f5bd494afa6ebb51452
tadeu390 <tadeu5552010@hotmail.com>
2019-06-06 23:18:33 -0300

    Padrão de repositório adicionado a API. Criado também um diretório para a camada de serviço (regras de negócio).

---
+++ src/app/Repositories/Contracts/RepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Repositories\Contracts;
+
+interface RepositoryInterface
+{
+    public function getAll();
+    public function findById($id);
+    public function findWhere($column, $value);
+    public function findWhereNull($column);
+    public function findWhereFirst($column, $value);
+    public function paginate($totalPage = 10);
+    public function store(array $data);
+    public function update($id, array $data);
+    public function delete($id);
+    public function orderBy($column, $order = 'DESC');
+    public function groupBy($column);
+}
